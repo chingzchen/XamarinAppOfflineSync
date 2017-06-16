309e48841b15068ccb6c2e93df3ce569b6c1f4fb
chingch <chingch@CCSURFACE>
2017-06-16 17:21:00 +0800

    Remove connection

--- ccmobileapppoc/Constants.cs
+++ ccmobileapppoc/Constants.cs
@@ -5,11 +5,11 @@ namespace ccmobileapppoc
 	public static class Constants
 	{
         // Replace strings with your Azure Mobile App endpoint.
-        public static string ApplicationURL = @"https://ccmobileapppoc.azurewebsites.net";
-        public const string SenderID = "217712605294"; // Google API Project Number
-        public const string ListenConnectionString = "Endpoint=sb://ccmobileapppoc.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=jL09hLNJy6+qy5YDLuw3ytYsepFMgWXmJEU2+R9sGtU=";
-        public const string NotificationHubName = "ccmobileapppochub";
-        public const string TAG = "MyBroadcastReceiver-GCM";
+        public static string ApplicationURL = @"";
+        public const string SenderID = ""; // Google API Project Number
+        public const string ListenConnectionString = "";
+        public const string NotificationHubName = "";
+        public const string TAG = "";
     }
 }
 
